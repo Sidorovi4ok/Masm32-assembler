069ae50ca7649e74734fdae689080b1aede7f8d2
Dmitriy Kononenko <123237622+Sidorovi4ok@users.noreply.github.com>
2024-11-09 15:45:35 +0300

    Update and rename 6.asm to 03. Working with offset.asm

--- 03. Working with offset.asm
+++ 03. Working with offset.asm
@@ -1,34 +1,42 @@
 include \masm32\include\masm32rt.inc
 include <\masm32\include\debug.inc>
 includelib <\masm32\lib\debug.lib>
+
 .data
 v1 db 11
+
 .code
 start:
 mov al, v1
 shl al, 2
-;printf("%d\n", al)
 PrintHex al
+
 and al, 0
 mov al, v1
 shr al, 2
 PrintHex al
+
 and al, 0
 mov al, v1
 sal al, 2
 PrintHex al
+
 and al, 0
 mov al, v1
 sar al, 2
 PrintHex al
+
 and al, 0
 mov al, v1
 rol al, 2
 PrintHex al
+
 and al, 0
 mov al, v1
 ror al, 2
 PrintHex al
+
 inkey "Press any key!"
-invoke ExitProcess,0 ; выход в Windows
+invoke ExitProcess,0 ; ГўГ»ГµГ®Г¤ Гў Windows
+
 end start
